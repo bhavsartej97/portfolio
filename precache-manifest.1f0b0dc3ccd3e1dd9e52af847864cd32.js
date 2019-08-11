7453437e4c30581d6e1120b00af1292b6e0c803a
Tej Bhavsar <bhavsartej97@gmail.com>
2019-08-10 21:35:47 -0400

    Updates

---
+++ precache-manifest.1f0b0dc3ccd3e1dd9e52af847864cd32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44801184e1a32174d5c4ee03d4e98b3",
+    "url": "/portfolio/public/index.html"
+  },
+  {
+    "revision": "ae5ac03f7821f5069c80",
+    "url": "/portfolio/public/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/public/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ae5ac03f7821f5069c80",
+    "url": "/portfolio/public/static/js/main.8a741581.chunk.js"
+  },
+  {
+    "revision": "cbca3317afb6db88d3c9",
+    "url": "/portfolio/public/static/js/runtime~main.3167a4f0.js"
+  }
+]);
